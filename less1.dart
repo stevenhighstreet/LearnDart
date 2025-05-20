4087c379389855d27187ae3a39ac1457296ff753
unknown <tinhvan710@gmail.com>
2025-05-20 22:12:49 +0700

    Initial commit

---
+++ less1.dart
@@ -0,0 +1,15 @@
+void main(){
+  int mem = 100;
+  String mes = 'welcome flutter world ' + mem.toString();
+
+  print(mes);
+  print("Hello Dart");
+  doSomething();
+}
+
+void doSomething() {
+  var w=2025;
+  var xtest = "something in the world $w";
+
+  print(xtest);
+}
